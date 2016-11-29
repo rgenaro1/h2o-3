cb36d1503280a1dc1e135891fb511cdb7f581fc5
wendycwong <wendycwong@yahoo.com>
2016-11-29 12:36:52 -0800

    Check runit_RDoc_gbm run time.  Modified repeated runs.

--- h2o-r/tests/testdir_docexamples/runit_Rdoc_gbm_timing.R
+++ h2o-r/tests/testdir_docexamples/runit_Rdoc_gbm_timing.R
@@ -6,7 +6,7 @@ source("../../scripts/h2o-r-test-setup.R")
 # for GLRM timing test.
 test.RdocGBM.golden <- function() {
 
-    num_runs = 1  # no need to be hugh, it happens about one run in six, I was told
+    num_runs = 20  # no need to be hugh, it happens about one run in six, I was told
     ausPath <- locate("smalldata/extdata/australia.csv")
     australia.hex <- h2o.uploadFile(path = ausPath)
     independent<- c("premax", "salmax","minairtemp", "maxairtemp", "maxsst", "maxsoilmoist", "Max_czcs")
@@ -24,7 +24,6 @@ test.RdocGBM.golden <- function() {
     for(i in 1:num_runs){
         gaussian_model = h2o.gbm(y = dependent, x = independent, training_frame = australia.hex, ntrees = 10, max_depth = 3, min_rows = 2, learn_rate = 0.2, distribution= "gaussian")
         results_g = rbind(results_g, c(gaussian_model@parameters$seed, gaussian_model@model$run_time))
-        print(results_g)
     }
     print(results_g)
 
